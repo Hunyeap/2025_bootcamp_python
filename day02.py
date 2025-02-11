42f068699c062215eeac0aa27546e31b6e9fed56
hunyeap <choihunyeap@gmail.com>
2025-02-11 10:34:41 +0900

    update

--- day02.py
+++ day02.py
@@ -1,3 +1,7 @@
 for dan in range(2, 10, 1):
     for i in range(1, 10, 1):
         print(f"{dan} x {i} = {dan * i}")
+
+dan = int(input("input dan: "))
+for i in range(1, 10, 1):
+    print(f"{dan} x {i} = {dan * i}")
