9865b3eb414d78973709911d3a7b2217c1279faa
hunyeap <choihunyeap@gmail.com>
2025-02-11 13:30:42 +0900

    add function

--- day02.py
+++ day02.py
@@ -8,19 +8,20 @@ import math
 # for i in range(1, 10, 1):
 #     print(f"{dan} x {i} = {dan * i}")
 
-n = int(input("input n: "))
-is_prime = True
-
-if n == 1:
-    is_prime = False
-for i in range(2, int(math.sqrt(n))+1):
-    if n % i == 0:
-        is_prime = False
-        print(f"{i} ")
-if is_prime:
-    print(f"{n} is a prime number")
-else:
-    print(f"{n} is not a prime number")
+# n = int(input("input n: "))
+# is_prime = True
+#
+# if n == 1:
+#     is_prime = False
+# for i in range(2, int(math.sqrt(n))+1):
+#     if n % i == 0:
+#         is_prime = False
+#         print(i, end=" ")
+#         break
+# if is_prime:
+#     print(f"{n} is a prime number")
+# else:
+#     print(f"{n} is not a prime number")
 
 
 
@@ -40,3 +41,24 @@ else:
 #     print(f"{n} is a prime number")
 # else:
 #     print(f"{n} is not a prime number")
+
+def is_prime(num):
+    """
+    This function checks if a number is prime.
+    if a number is prime, it returns True.
+    """
+    if num >= 2:
+        for i in range(2, int(math.sqrt(num))+1):
+            if num % i == 0:
+                return False
+    else:
+        return False
+    return True
+
+
+n = int(input("input n: "))
+
+if is_prime(n):
+    print(f"{n} is a prime number")
+else:
+    print(f"{n} is not a prime number")
