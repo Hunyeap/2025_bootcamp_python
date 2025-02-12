c0ca2b3e53cb40c4b599f6b6fbee20c74572d688
hunyeap <choihunyeap@gmail.com>
2025-02-12 16:43:36 +0900

    upload day02

--- day02.py
+++ day02.py
@@ -1,7 +1,15 @@
-# Assignment Day 02
-# v1.4) Make my_pow custom function instead of ** operator, power function and make it work.
 import math
+# v1.1) for -> while
+# v1.2) while 구문으로 구간 소수를 출력하는 프로그램을 작성
+# v1.3) ** 대신 pow 함수 사용하기
+# v1.4) pow 대신 함수 만들어 사용하기
+def my_sqrt(a) -> float:
+    epsilon = 1e-10
+    guess = a
+    while abs(guess * guess - a) > epsilon:
+        guess = 0.5 * (guess + a / guess)  # 뉴턴-랩슨 공식 적용
 
+    return guess
 
 def my_pow(b, e) -> float:
     """
@@ -28,34 +36,43 @@ def my_pow(b, e) -> float:
     return result
 
 
-def is_prime(num) -> bool:
+# def my_pow(a, b) -> float:
+#     """
+#     This function returns a^b
+#     :param a: base number
+#     :param b: exponent
+#     """
+#     result = 1
+#     for k in range(b):
+#         result *= a
+
+
+def is_prime(num):
     """
-    A function that returns True if it is a prime number and False if it is not a prime number
-    :param num: integer number
-    :return: boolean type
+    This function checks if a number is prime.
+    If a number is prime, it returns True.
     """
-    if num >= 2:
-        i = 2
-        while i < (int(my_pow(num, 0.5)) + 1):
-        #while i*i < num+1:
-            if num % i == 0:
-                return False
-            i = i + 1
-    else:
+    if num == 1:
         return False
+
+    i = 2
+    while i <= int(my_pow(num, 0.5)):
+        if num % i == 0:
+            return False
+        i += 1
+
     return True
 
 
-#print(my_pow(2, 9))
-numbers = input("Input number : ").split()  # ex) 900 1000
+numbers = input("input n: ").split()
 n1 = int(numbers[0])
 n2 = int(numbers[1])
 
 if n1 > n2:
     n1, n2 = n2, n1
 
-j = n1
-while j <= n2:
-    if is_prime(j):
-        print(j, end=' ')
-    j = j + 1
+current = n1
+while current <= n2:
+    if is_prime(current):
+        print(current, end=" ")  # 한 줄에 출력
+    current += 1
