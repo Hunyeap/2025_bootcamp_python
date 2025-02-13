60be943342c1b45c3e57894df2b72ac7f2027d50
hunyeap <choihunyeap@gmail.com>
2025-02-13 16:40:43 +0900

    v1.2

--- day04.py
+++ day04.py
@@ -1,52 +1,45 @@
-import random
+# Assignment
+# v3.6) 2중 데코레이터 적용. 성능측정 데코레이터, 디스크립션 데코레이터를 팩토리얼 함수에 적용하시오.
+import time
 
-# d_s_p = {"위스키" : ['초콜릿', 50_000]}
-# print(d_s_p["위스키"][1])
-
-drinks = ["위스키", "와인", "소주", "고량주"]
-snacks = ["초콜릿", "치즈", "삽겹살", "양꼬치"]
-prices = [50000, 30000, 5000, 7500]
-amounts = [0 for i in range(len(drinks))]
-
-drinks.append("사케")
-snacks.append("광어회")
-prices.append(25000)
-amounts.append(0)
-snacks[0] = "낙곱새"
-# drinks.append("데킬라")
-# snacks.append("소금")
-# prices.append(35000)
-# amounts.append(0)
-
-total_price = 0
-
-def print_menu_total_price(n):
-    global total_price
-    print(f'{drinks[n]}에 어울리는 안주는 {snacks[n]} 입니다')
-    print(f'가격 : {prices[n]}')
-    amounts[n] = amounts[n] + 1
-    total_price = total_price + prices[n]
+def description_decorator(func):
+    def wrapper(*arg):
+        print(func.__name__)
+        print(func.__doc__)
+        r = func(*arg)
+        return r
+    return wrapper
 
 
-menu_list = '다음 술중에 고르세요.\n'
-for i in range(len(drinks)):
-    menu_list = menu_list + f'{i+1}) {drinks[i]}  '
-menu_list = menu_list + f'{len(drinks)+1}) 아무거나  {len(drinks)+2}) 종료 : '
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'실행시간 : {e - s}초')
+        return r
+    return wrapper
 
-while True:
-    menu = int(input(menu_list))
-    if 1 <= menu <= len(drinks):
-        print_menu_total_price(menu - 1)
-    elif menu == len(drinks)+1:
-        random_index = random.randint(0, len(drinks)-1)
-        print_menu_total_price(drinks[random_index])
-    elif menu == len(drinks)+2:
-        print(f'다음에 또 오세요')
-        break
+@time_decorator
+@description_decorator
+def factorial_repetition(n) -> int:
+    """
+    factorial function by loop
+    :param n:
+    :return: results of factorial operation
+    """
+    result = 1
+    for i in range(2, n+1):
+        result = result * i
+    return result
 
 
-for k in range(len(drinks)):
-    if amounts[k] != 0:
-        print(f"주류명 : {drinks[k]}\n 수량 : {amounts[k]:>2} "
-              f"단가 : {prices[k]:>6}  소계 : {prices[k] * amounts[k]:>5}")
-print(f"총 금액 : {total_price}원")
+number = int(input())
+print(f"{number}! = {factorial_repetition(number)}")
+
+
+# number = int(input())
+# ft = time_decorator(factorial_repetition)
+# print(f"{number}! = {ft(number)}")
+# number = int(input())
+# print(f"{number}! = {factorial_repetition(number)}")
