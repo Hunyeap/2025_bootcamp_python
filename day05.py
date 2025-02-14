583013babf08580e2bf668e90fe417e0cfbf9af8
hunyeap <choihunyeap@gmail.com>
2025-02-14 10:29:16 +0900

    v1.1

--- day05.py
+++ day05.py
@@ -1,13 +1,23 @@
-# def my_range(first=0, last=5, step=1):
-#     number = first
-#     while number < last:
-#         yield number
-#         number += step
-#
-# r = my_range()
-# print(r, type(r))
-#
-# for x in r:
-#     print(x)
-# for x in r:
-#     print(x)
+# y = 3
+# x = y * 9
+# z = list(range(x))
+# print(z)
+# print(tuple(map(str, z)))
+
+def my_range(first=0, last=5, step=1):
+    number = first
+    while number < last:
+        yield number
+        # number += step
+        number = number + step
+
+
+r = my_range()
+print(r, type(r))
+
+for x in r:
+    print(x)
+for x in r:
+    print(x)
+for x in my_range(10, 19, 2):
+    print(x)
