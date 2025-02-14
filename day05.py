e10e348b4d5d3860adf5369172f1eac40d0ceea6
hunyeap <choihunyeap@gmail.com>
2025-02-14 11:10:21 +0900

    v1.3

--- day05.py
+++ day05.py
@@ -4,20 +4,34 @@
 # print(z)
 # print(tuple(map(str, z)))
 
-def my_range(first=0, last=5, step=1):
-    number = first
-    while number < last:
-        yield number
-        # number += step
-        number = number + step
+# def my_range(first=0, last=5, step=1):
+#     number = first
+#     while number < last:
+#         yield number
+#         # number += step
+#         number = number + step
+#
+#
+# r = my_range()
+# print(r, type(r))
+#
+# for x in r:
+#     print(x)
+# for x in r:
+#     print(x)
+# for x in my_range(10, 19, 2):
+#     print(x)
 
+def fibonacci_recursion(n) -> int:
+    """
+    fibonacci with recursion
+    This function will return the nth fibonacci number
+    """
+    if n <= 0 :
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursion(n - 1) + fibonacci_recursion(n - 2)
 
-r = my_range()
-print(r, type(r))
-
-for x in r:
-    print(x)
-for x in r:
-    print(x)
-for x in my_range(10, 19, 2):
-    print(x)
+print(fibonacci_recursion(int(input())))
