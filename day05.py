0a8013b1e6c4de302fc886ebf142a095bc889815
hunyeap <choihunyeap@gmail.com>
2025-02-14 11:19:47 +0900

    day05.py countdown

--- day05.py
+++ day05.py
@@ -1,37 +1,21 @@
-# y = 3
-# x = y * 9
-# z = list(range(x))
-# print(z)
-# print(tuple(map(str, z)))
+def countdown_loop(n):
+    for i in range(n, -1, -1):
+        if i == 0:
+            print("펑~")
+        else:
+            print(i)
 
-# def my_range(first=0, last=5, step=1):
-#     number = first
-#     while number < last:
-#         yield number
-#         # number += step
-#         number = number + step
-#
-#
-# r = my_range()
-# print(r, type(r))
-#
-# for x in r:
-#     print(x)
-# for x in r:
-#     print(x)
-# for x in my_range(10, 19, 2):
-#     print(x)
 
-def fibonacci_recursion(n) -> int:
-    """
-    fibonacci with recursion
-    This function will return the nth fibonacci number
-    """
-    if n <= 0 :
-        return 0
-    elif n == 1:
-        return 1
+def countdown_recursion(n):
+    if n < 0:
+        return
+    if n == 0:
+        print("펑!")
     else:
-        return fibonacci_recursion(n - 1) + fibonacci_recursion(n - 2)
+        print(n)
+    countdown_recursion(n-1)
 
-print(fibonacci_recursion(int(input())))
+
+n = int(input())
+#countdown_loop(n)
+countdown_recursion(n)
